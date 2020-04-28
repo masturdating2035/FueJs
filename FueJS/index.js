1e4c01475a8344a91270d20a426f52329fbfc08e
Ali Ghodousi <48363598+masturdating1991@users.noreply.github.com>
2020-04-29 04:08:06 +0430

    Update index.js

--- FueJS/index.js
+++ FueJS/index.js
@@ -1,17 +1,33 @@
-const FueJs = {};
+const FueJS = {};
 
-
-FueJs.createElement = function (type, props, ...children) {
-    const element = document.createElement(type);
-    Object.assign(element.style, props.style);
-    if (children) {
-        for (let i=0;i<children.length;i++){
-            const child = children[i]
-            element.append(child)
-        }
-    }
-    return element
+FueJS.createTextNode = function (string) {
+    return document.createTextNode(string);
 }
 
-
-
+FueJS.createElement = function (type, props, children) {
+    const element = document.createElement(type);
+    props = props || {};
+    for (let index = Object.keys(props).length - 1; index >= 0; index--) {
+        const propKey = Object.keys(props)[index];
+        if (propKey === 'style') {
+            Object.assign(element.style, props.style);
+        } else if (propKey === 'className') {
+            element.className = props.className;
+        } else if (typeof props[propKey] === 'function') {
+            element[propKey] = props[propKey];
+        } else {
+            element.setAttribute(propKey, props[propKey]);
+        }
+    }
+    if (children) {
+        if (Array.isArray(children)) {
+            for (let index = children.length - 1; index >= 0; index--) {
+                const child = children[index];
+                element.prepend(typeof child === 'string' ? this.createTextNode(child) : child);
+            }
+        } else {
+            element.append(typeof children === 'string' ? this.createTextNode(children) : children);
+        }
+    }
+    return element;
+}
